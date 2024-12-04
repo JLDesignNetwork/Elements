b0f5d3a5d0a37ec72cf596ac8008a8bed513b735
JLDN Admin <59946223+JLDesignNetwork@users.noreply.github.com>
2024-12-04 16:40:02 +0000

    code refactoring

--- _src/script.js
+++ _src/script.js
@@ -1,34 +1,29 @@
-const fetchElements = () => {
-	return $(document).find('div.jldn-meter, div.jldn-button, button.jldn-button, code.jldn-code');
-}
-const buildElement = (el,el_id,el_class,el_options) => {
-	const allowed_options = ['style','shape','width','height','fill-color','candystripe-color','theme'];
-	const alt_allowed_options = $.merge(allowed_options, ['reveal-color','fill-size','animation-speed']);
-	const selector = `${el.toLowerCase()}#${el_id}.${el_class}`;
-
-  switch (el_class) {
-    case 'jldn-meter':
-      return `<div id="${el_id}" class="${el_class}" ${el_options? Object.keys(el_options).map(k => `${k}="${el_options[k]}"`).join(' ') : ''}></div>`;
-    case 'jldn-button':
-    case 'button.jldn-button':
-      return `<div id="${el_id}" class="${el_class}" ${el_options? Object.keys(el_options).map(k => `${k}="${el_options[k]}"`).join(' ') : ''}></div>`;
-    case 'code.jldn-code':
-      return `<code id="${el_id}" class="${el_class}" ${el_options? Object.keys(el_options).map(k => `${k}="${el_options[k]}"`).join(' ') : ''}></code>`;
-  }
-  if (el_class == 'jldn-meter') console.log(alt_allowed_options);
-}
+import { Elements } from './modules/elements.js';
 
 $(() => {
-	// search document for all elements marked for rebuilding
-  jQuery.each(fetchElements(), (index,value) => {
-  	let el = $(value).prop("tagName");
-  	let el_id = $(value).attr('id');
-    let el_class = $(value).attr('class');
-	  let el_options = $(value).data('options');
-    
-  	//console.log([el,el_id,el_class,el_options]);
-    
+  let JLDN = new Elements()
+
+  // search document for all elements marked for rebuilding
+  jQuery.each(JLDN.fetchElements(), (index, value) => {
+    let el = $(value).prop("tagName")
+    let el_id = $(value).attr("id")
+    let el_class = $(value).attr("class")
+    let el_options = $(value).data("options")
+    //console.log(el_options);
+
     // build found elements
-    buildElement(el,el_id,el_class,el_options);
-  });
-});
+    JLDN.buildElement(el, el_id, el_class, el_options)
+  })
+
+  $(".meter > span").each(function () {
+    $(this)
+      .data("origWidth", ($(this).width() / $(this).parent().width()) * 100)
+      .width(0)
+      .animate(
+        {
+          width: $(this).data("origWidth") + "%",
+        },
+        3600,
+      )
+  })
+})
